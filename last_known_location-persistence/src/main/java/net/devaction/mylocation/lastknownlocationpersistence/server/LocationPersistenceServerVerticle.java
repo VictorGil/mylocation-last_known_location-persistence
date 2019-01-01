70f7c1e11bf07eb0312f338d5822d227726747e5
VictorGil <victorgilsanz@gmail.com>
2019-01-01 10:13:05 +0100

    Replacing "LocationPersistenceServerHandler.class" by "handler.getClass()"

--- last_known_location-persistence/src/main/java/net/devaction/mylocation/lastknownlocationpersistence/server/LocationPersistenceServerVerticle.java
+++ last_known_location-persistence/src/main/java/net/devaction/mylocation/lastknownlocationpersistence/server/LocationPersistenceServerVerticle.java
@@ -41,7 +41,7 @@ public class LocationPersistenceServerVerticle extends AbstractVerticle implemen
         
         consumer.completionHandler(asyncResult -> {
             if (asyncResult.succeeded()) {
-                log.info("The " + LocationPersistenceServerHandler.class.getSimpleName() + 
+                log.info("The " + handler.getClass().getSimpleName() + 
                         " has been successfully registered ans it started listening for messages on " + address + " event bus address.");
             } else {
                 log.error("FATAL: Registration of " + LocationPersistenceServerHandler.class.getSimpleName() + " has failed.");
